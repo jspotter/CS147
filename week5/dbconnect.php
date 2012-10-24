569e857ee6811c8f51144e5614b958f7c7ff7b8d
Jonathan Potter <potterjs61490@gmail.com>
2012-10-24 15:34:57 -0700

    woo

---
+++ week5/dbconnect.php
@@ -0,0 +1,14 @@
+<?php
+  $dbname = "./project.db";
+  try
+  {
+    $db = new PDO("sqlite:" . $dbname);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }
+  catch (PDOException $e)
+  {
+    "SQLite connection failed: " . $e->getMessage();
+    exit();
+  }
+?>
+
---
+++ week5/footer.php
@@ -0,0 +1,10 @@
+<div data-role="footer" data-id="samebar" class="nav-glyphish-example" data-position="fixed" data-tap-toggle="false">
+	<div data-role="navbar" class="nav-glyphish-example" data-grid="c">
+	<ul>
+		<li><a href="index.php" id="home" data-icon="custom" class="<?= $homeClass ?>">Home</a></li>
+		<li><a href="login.php" id="key" data-icon="custom" class="<?= $loginClass ?>">Login</a></li>
+		<li><a href="filter.php" id="beer" data-icon="custom" class="<?= $filterClass ?>">Filter</a></li>
+		<li><a href="#" id="skull" data-icon="custom" class="<?= $settingsClass ?>">Settings</a></li>
+	</ul>
+	</div>
+</div>
