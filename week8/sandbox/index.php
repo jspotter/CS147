653e3b5b0d81ab3f6b481f85d61e2b3a1f2eaa9f
Jonathan Potter <potterjs61490@gmail.com>
2012-11-14 14:46:43 -0800

    Redirect completed

--- week8/sandbox/index.php
+++ week8/sandbox/index.php
@@ -1,3 +1,12 @@
+<?php
+	include 'Mobile_Detect.php';
+	$detect = new Mobile_Detect();
+
+	if (!$detect->isMobile()) {
+		header("Location: mobiletest/iphone.php?url=stanford.edu/~jspotter/cgi-bin/cs147/CS147/week8/sandbox/mobile.php");
+	}
+?>
+
 <!DOCTYPE html> 
 <html>
 
